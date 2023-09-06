5850b364caebc1f1a5651c944a62ec05c266a388
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-09-05 20:42:36 -0500

    initial commit

---
+++ MovieListMiles/Models/MovieContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieListMiles.Models
+{
+    public class MovieContext : DBContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; } = null;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(){
+
+                new Movie
+                {
+                    MovieID = 1,
+                    Name = "Casablanca",
+                    Year = 1942,
+                    Rating = 5
+                },
+                  new Movie
+                  {
+                      MovieID = 2,
+                      Name = "Wonder Woman",
+                      Year = 2017,
+                      Rating = 3
+                  },
+                    new Movie
+                    {
+                        MovieID = 3,
+                        Name = "Moonstruck",
+                        Year = 1988,
+                        Rating = 4
+                    }
+
+
+
+
+            };
+        }
+    }
+}
